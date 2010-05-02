bfc2f6cf953dfa980579b15c1199effdac866254
Dan Callaghan <djc@djc.id.au>
2010-05-02 13:35:57 +1000

    useful utility method

--- src/main/java/au/com/miskinhill/rdftemplate/XMLStream.java
+++ src/main/java/au/com/miskinhill/rdftemplate/XMLStream.java
@@ -1,12 +1,29 @@
 package au.com.miskinhill.rdftemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.events.XMLEvent;
 
 public class XMLStream implements Iterable<XMLEvent> {
+    
+    public static XMLStream collect(Iterator<XMLEvent> it) {
+        List<XMLEvent> events = new ArrayList<XMLEvent>();
+        while (it.hasNext()) {
+            XMLEvent event = it.next();
+            switch (event.getEventType()) {
+                case XMLStreamConstants.START_DOCUMENT:
+                case XMLStreamConstants.END_DOCUMENT:
+                    break; // discard
+                default:
+                    events.add(event);
+            }
+        }
+        return new XMLStream(events);
+    }
     
     private final List<XMLEvent> events;
     
